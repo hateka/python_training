8b2552bbd5ff6754d2785552cd4032532f97a8d0
okui <okui12@gmail.com>
2016-04-25 15:36:41 +0900

    refactoring

--- python_training/1-8.py
+++ python_training/1-8.py
@@ -4,21 +4,21 @@ param = sys.argv
 end = len(param)
 
 for i in range(1, end):
-	param[1] = int(param[1])
-	param[2] = int(param[2])
-	param[3] = int(param[3])
-	param[4] = int(param[4])
-	param[5] = int(param[5])
+	param[i] = int(param[i])
 
-	if param[1] < 0 or param[2] < 0 or param[1] > 100 or param[2] > 100:
-		print 'param is valid'
-	if param[3] < 0 or param[4] < 0 or param[3] > 100 or param[4] > 100 or param[5] > 100:
-		print 'param is valid'
+	if i < 5:
+		if param[i] < 0 or param[i] > 100:
+			print 'param is valid'
+	else:
+		if  param[i] > 100:
+			print 'param is valid'
+	W = int(param[1])
+	H = int(param[2])
 
-	w = param[3] + param[5]
-	h = param[4] + param[5]
-	
-	if w > param[1] or h > param[2]:
+	w = int(param[3]) + int(param[5])
+	h = int(param[4]) + int(param[5])
+
+	if w > W or h > H:
 		print 'NO'
 		break
 	else:
